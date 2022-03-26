e263b1c3afe5238c537304cbeee59b2f57888e80
Euan Mendoza <bebopbamf@effectfree.dev>
2022-03-26 12:19:37 +1100

    login form layout

--- src/IoTBay/Program.cs
+++ src/IoTBay/Program.cs
@@ -1,4 +1,5 @@
 using IoTBay.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,14 @@ builder.Services.AddDbContext<IoTBayDbContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+        options.SlidingExpiration = true;
+        options.AccessDeniedPath = "/Forbidden/";
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,7 +26,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+};
+
+app.UseCookiePolicy(new CookiePolicyOptions
+{
+    MinimumSameSitePolicy = SameSiteMode.Strict,
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
